f8ca1c2e15ad00cde6310ba4d816687f46c7529b
Pedro Rosales <pedro.rosales@thejabb.com>
2025-08-25 15:45:55 -0400

    WIP

--- functions/classes/dbClient.ts
+++ functions/classes/dbClient.ts
@@ -72,15 +72,15 @@ export default class Database {
     return data as Query.Response;
   };
 
-  // public static async login(email: string, password: string) {
-  //   const connection = Database.connection();
-  //   let { data, error } = await connection.auth.signInWithPassword({
-  //     email,
-  //     password,
-  //   });
+  public static async login(email: string, password: string) {
+    const connection = Database.connection();
+    let { data, error } = await connection.auth.signInWithPassword({
+      email,
+      password,
+    });
 
-  //   if (error) return error;
+    if (error) return error;
 
-  //   return data;
-  // }
+    return data;
+  }
 }
